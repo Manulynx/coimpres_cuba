a1552f226321d69f14360307f3059396083dced3
Manulynx <lynxiyo@gmail.com>
2025-10-16 15:15:35 -0400

    collectstatic

--- coimpres_cuba/settings.py
+++ coimpres_cuba/settings.py
@@ -120,8 +120,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'coimpres_cuba/static'),
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # For collectstatic command
+STATICFILES_DIRS = [BASE_DIR / "coimpres_cuba" / "static"]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
