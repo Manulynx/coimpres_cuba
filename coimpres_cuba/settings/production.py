ad0f486747225b70fd57faa13442313c4b98eb1c
Manulynx <lynxiyo@gmail.com>
2025-11-17 17:40:54 -0500

    MYSQL

--- coimpres_cuba/settings/production.py
+++ coimpres_cuba/settings/production.py
@@ -7,10 +7,10 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tu_usuario$nombrebase',
-        'USER': 'tu_usuario',
-        'PASSWORD': 'tu_password',
-        'HOST': 'tu_usuario.mysql.pythonanywhere-services.com',
+        'NAME': 'Coimpre$nombrebase',
+        'USER': 'Coimpre',
+        'PASSWORD': 'Manuel.980317',
+        'HOST': 'Coimpre.mysql.pythonanywhere-services.com',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
